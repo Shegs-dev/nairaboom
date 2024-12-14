b8e0c0f7ce167cae446a3d9cfe4373be1c834c4b
Segun Ajayi <fccinborn@gmail.com>
2024-12-14 07:00:48 +0100

    push to Daniel

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
